9a6ff160e4d70cb15c7496491b507fc9f5b56349
moony <78634780+ysy56@users.noreply.github.com>
2024-04-29 17:18:58 +0900

    feat[level 1]: 사칙연산 기호 입력 받기

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -16,5 +16,9 @@ public class App {
         // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
         int secondNum = sc.nextInt();
         sc.nextLine();
+
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
+        char operator = sc.nextLine().charAt(0);
     }
 }
