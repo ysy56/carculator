66f1b78871535993aa353f98959c02544c9a1b92
moony <78634780+ysy56@users.noreply.github.com>
2024-04-29 17:42:57 +0900

    feat[level 1]: 4번 - "exit" 입력 전까지 반복

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class App {
@@ -7,41 +8,50 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-        int firstNum = sc.nextInt();
-        sc.nextLine();
 
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-        int secondNum = sc.nextInt();
-        sc.nextLine();
+        while (true) { // 반복문 사용 해서 연산을 반복
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+            int firstNum = sc.nextInt();
+            sc.nextLine();
 
-        System.out.print("사칙연산 기호를 입력하세요: ");
-        // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
-        char operator = sc.nextLine().charAt(0);
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+            int secondNum = sc.nextInt();
+            sc.nextLine();
 
-        int result = 0;
-        /* switch를 이용하여 사칙연산 기호에 따른 계산 실행*/
-        switch (operator) {
-            case '+':
-                result = firstNum + secondNum;
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
+            char operator = sc.nextLine().charAt(0);
+
+            int result = 0;
+            /* switch를 이용하여 사칙연산 기호에 따른 계산 실행*/
+            switch (operator) {
+                case '+':
+                    result = firstNum + secondNum;
+                    break;
+                case '-':
+                    result = firstNum - secondNum;
+                    break;
+                case '*':
+                    result = firstNum * secondNum;
+                    break;
+                case '/':
+                    if (secondNum == 0) // 분모에 0이 입력될 경우 예외문구 출력
+                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                    else
+                        result = firstNum / secondNum;
+                    break;
+                default:
+                    System.out.println("사칙연산 기호는 +, -, *, / 중에서 입력할 수 있습니다.");
+            }
+            System.out.println("결과: " + result);
+
+            // exit을 입력 받으면 반복 종료
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            String answer = sc.nextLine();
+            if (Objects.equals(answer, "exit"))
                 break;
-            case '-':
-                result = firstNum - secondNum;
-                break;
-            case '*':
-                result = firstNum * secondNum;
-                break;
-            case '/':
-                if (secondNum == 0) // 분모에 0이 입력될 경우 예외문구 출력
-                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                else
-                    result = firstNum / secondNum;
-                break;
-            default:
-                System.out.println("사칙연산 기호는 +, -, *, / 중에서 입력할 수 있습니다.");
         }
-        System.out.println("결과: " + result);
     }
 }
