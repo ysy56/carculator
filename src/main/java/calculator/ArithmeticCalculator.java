b895985ce3ef404f5a5924ad15ea98598d6703c3
moony <78634780+ysy56@users.noreply.github.com>
2024-05-03 12:27:14 +0900

    refactor[level 2]: Divide 클래스 예외처리 추가 및 operatorFactory 메서드 생성

--- src/main/java/calculator/ArithmeticCalculator.java
+++ src/main/java/calculator/ArithmeticCalculator.java
@@ -14,25 +14,19 @@ public class ArithmeticCalculator extends Calculator{
     // 계산을 수행하는 메서드
     @Override
     double calculate() throws Exception {
-        double result = 0;
+        return this.operate.operate(this.firstNum, this.secondNum); // 연산 수행 후 결과 반환
+    }
+
+    private void operatorFactory() throws Exception {
         /* switch를 이용하여 사칙연산 기호에 따른 계산 실행*/
         switch (operatorType) {
             case PLUS -> setOperator(new AddOperator());
             case MINUS -> setOperator(new SubtractOperator());
             case MULTIPLY -> setOperator(new MultiplyOperator());
-            case DIVIDE -> {
-                if (this.secondNum == 0) // 분모에 0이 입력될 경우 예외문구 출력
-                    throw new Exception("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                else
-                    setOperator(new DivideOperator());
-            }
+            case DIVIDE -> setOperator(new DivideOperator());
             case MOD -> setOperator(new ModOperator());
             default -> throw new Exception("+, -, *, /가 아닌 사칙연산은 입력할 수 없습니다.");
         }
-        // 연산 수행 후 결과 반환
-        result = this.operate.operate(this.firstNum, this.secondNum);
-
-        return result;
     }
 
     // 숫자와 연산자를 설정하는 메서드
--- src/main/java/calculator/DivideOperator.java
+++ src/main/java/calculator/DivideOperator.java
@@ -3,6 +3,8 @@ package calculator;
 public class DivideOperator implements Operate{
     @Override
     public int operate(int firstNum, int secondNum) {
+        if (secondNum == 0) // 분모에 0이 입력될 경우 예외문구 출력
+            throw new ArithmeticException("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
         return firstNum / secondNum;
     }
 }
